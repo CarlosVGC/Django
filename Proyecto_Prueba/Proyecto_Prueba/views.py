16da9963209add73e8fafe724e2836cfd36c94fa
Carlos Venegas <awesomethebest@hotmail.es>
2021-06-23 23:48:52 -0500

    actualizacion archivos

--- Proyecto_Prueba/Proyecto_Prueba/views.py
+++ Proyecto_Prueba/Proyecto_Prueba/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse
 import datetime
+from django.template import Template, Context
+
 
 def saludo(request): #Primera vista
-    documento = "<html><body><h1>Esta es la primera pagina con django</h1></body></html>"
+    doc_externo = open("Proyecto_Prueba/platillas/plantillasaludo.html") #se abre un doc htmll ext se puede poner toda la ruta o cortada como se muestra
+    plt = Template(doc_externo.read())  #Se crea la plantilla del documento externo
+    doc_externo.close() # se cierra el doc externo
+    ctx = Context() # se crea el contexto
+
+    documento = plt.render(ctx) # se renderiza el documento
     return HttpResponse(documento)
+
 
 def despedida(request):
     return HttpResponse("Adios, que le vaya bien")
