8786e4542259074e90bfcb13863686317fe5a198
Carlos Venegas <awesomethebest@hotmail.es>
2021-06-27 23:59:19 -0500

    actualizacion archivos

--- Proyecto_Prueba/Proyecto_Prueba/views.py
+++ Proyecto_Prueba/Proyecto_Prueba/views.py
@@ -2,14 +2,21 @@ from django.http import HttpResponse
 import datetime
 from django.template import Template, Context
 
+class Persona(object):
+    def __init__(self,nombre,apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+
 
 def saludo(request): #Primera vista
     doc_externo = open("Proyecto_Prueba/platillas/plantillasaludo.html") #se abre un doc htmll ext se puede poner toda la ruta o cortada como se muestra
 
+    p1 = Persona("Dulce", "Carolina")
     nombre = "Dulce"
+    hora_actual = datetime.datetime.now()
     plt = Template(doc_externo.read())  #Se crea la plantilla del documento externo
     doc_externo.close() # se cierra el doc externo
-    ctx = Context({"nombre_persona": nombre }) # se crea el contexto
+    ctx = Context({"nombre_persona": nombre, "momento_actual": hora_actual, "nombre_clase": p1.nombre, "apellido_clase": p1.apellido}) # se crea el contexto
 
     documento = plt.render(ctx) # se renderiza el documento
     return HttpResponse(documento)
